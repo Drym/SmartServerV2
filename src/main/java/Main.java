096736594860c2ca8b3a361a5acdbec3e38068ad
Lucas Sauvage <lucas.sauvage.06@gmail.com>
2016-11-24 19:25:05 +0100

    Parsing de la réponse de l'API et renvoit a l'application en forme

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,11 @@ import com.mashape.unirest.http.exceptions.UnirestException;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import spark.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
 import static spark.Spark.*;
 
 public class Main {
@@ -13,26 +18,41 @@ public class Main {
     public static String KEY = "AIzaSyCAyS6YwjjNKyUdiITmjhd1dKc0swsw9E0";
     public static String SNAP_ROAD_URL = "https://roads.googleapis.com/v1/snapToRoads";
 
-    public static String json_test = "{\"value\":[{\"lg\":\"7.07532013\",\"lt\":\"43.62025872\"}]}";
+    public static String json_test = "{\"value\":[{\"lg\":\"7.07532013\",\"lt\":\"43.62025872\"}, {\"lg\":\"7.07532013\",\"lt\":\"43.62025872\"}, {\"lg\":\"7.07532013\",\"lt\":\"43.62025872\"}]}";
 
     public static void main(String[] args) {
+
         JSONObject test = new JSONObject(json_test);
         try {
             getCheckpoint(test);
         } catch (UnirestException e) {
             e.printStackTrace();
         }
+
         staticFileLocation("/public");
         port(7777);
         post("/checkpoint", Main::getCoord);
     }
 
     private static String getCoord(Request request, Response response) {
-        System.out.println(request.body());
-        return "coucou ca va encule";
+
+        System.out.println("Received from SmartRoad app : "+request.body());
+
+        JSONObject resultat = new JSONObject(request.body());
+        try {
+            resultat = getCheckpoint(resultat);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Google API resulat : "+resultat.toString());
+        String resultatFinal = parseJSON(resultat).toString();
+
+        return resultatFinal;
+        //return "coucou ca va encule";
     }
 
-    private static void getCheckpoint(JSONObject locations) throws UnirestException {
+    private static JSONObject getCheckpoint(JSONObject locations) throws UnirestException {
         String path = "?path=";
         JSONArray tab = locations.getJSONArray("value");
         int len = tab.length();
@@ -44,11 +64,42 @@ public class Main {
             }
         }
         String url = SNAP_ROAD_URL + path +"&key=" + KEY;
-        System.out.println(url);
-        HttpResponse<JsonNode> request = Unirest.get(url)
-                .asJson();
+        //System.out.println(url);
+        HttpResponse<JsonNode> request = Unirest.get(url).asJson();
         JSONObject myObj = request.getBody().getObject();
-        System.out.println(myObj.toString());
+
+        //System.out.println(myObj.toString());
+
+        return myObj;
     }
     // 7.07532013,43.62025872 43.62020459039435,7.074930474126736
+
+    private static JSONObject parseJSON(JSONObject resultat) {
+
+        JSONObject resultatFinal = new JSONObject();
+        Collection<JSONObject> items = new ArrayList<JSONObject>();
+
+        JSONArray array = resultat.getJSONArray("snappedPoints");
+        for (int i = 0; i < array.length(); i++) {
+            JSONObject jsonobject = array.getJSONObject(i);
+            //System.out.println(jsonobject);
+
+            JSONObject location = jsonobject.getJSONObject("location");
+            //System.out.println(location);
+
+            String lt =  location.get("latitude").toString();
+            String lg =  location.get("longitude").toString();
+            //System.out.println(lt + " "+ lg);
+
+            JSONObject item1 = new JSONObject();
+            item1.put("lt", lt);
+            item1.put("lg", lg);
+            items.add(item1);
+        }
+
+        resultatFinal.put("value", new JSONArray(items));
+        System.out.println("Parsing of Google API result : "+resultatFinal);
+
+        return resultatFinal;
+    }
 }
